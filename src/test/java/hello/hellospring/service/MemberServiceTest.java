143da50b07aea7a5382b3dbbf1d63f57715607b7
philipuuu <topgun0415@naver.com>
2023-01-23 23:59:36 +0900

    MemberServiceTest test pratice

---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,34 @@
+package hello.hellospring.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName())
+
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
