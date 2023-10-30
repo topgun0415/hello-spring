78c4c48540e6a7a4299e65de5069a7b0fe657cfe
philipuuu <topgun0415@naver.com>
2023-10-30 21:37:45 +0900

    Simple Controller with addAttribute

--- src/main/java/hello/hellospring/controller/helloController.java
+++ src/main/java/hello/hellospring/controller/helloController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class helloController {
 
+    // Mapping URL
     @GetMapping("hello")
     public String hello(Model model) {
-        model.addAttribute("data", "hello");
+        // attribute data to html "data" = "hello!!"
+        model.addAttribute("data", "spring!!");
+        // Return value is templates/hello.html
         return "hello";
     }
 }
