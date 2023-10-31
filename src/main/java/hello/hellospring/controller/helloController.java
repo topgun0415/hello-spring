e398022897725bf18a50f43c7c8dbe4143081a88
philipuuu <topgun0415@naver.com>
2023-11-01 03:16:31 +0900

    Spring Thymeleaf template engine practice

--- src/main/java/hello/hellospring/controller/helloController.java
+++ src/main/java/hello/hellospring/controller/helloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class helloController {
@@ -14,5 +15,12 @@ public class helloController {
         model.addAttribute("data", "spring!!");
         // Return value is templates/hello.html
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model) {
+        model.addAttribute("name", name);
+
+        return "hello-template";
     }
 }
